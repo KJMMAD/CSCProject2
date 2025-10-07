6f9c7fa56918087eeb29a1f99c41943a8f5c7ab4
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-07 17:38:15 -0400

    Added "miles" to cout line.

--- CSCProject2.cpp
+++ CSCProject2.cpp
@@ -4,5 +4,5 @@ int main() {
 	int gasTank = 20;
 	float mpgTown = 23.5;
 	float mpgHighway = 28.9;
-	std::cout << "A car with a 20 gallon gas tank can drive for " << gasTank * mpgTown << " in a town and " << gasTank * mpgHighway << " on the highway.";
+	std::cout << "A car with a 20 gallon gas tank can drive for " << gasTank * mpgTown << " miles in a town and " << gasTank * mpgHighway << " miles on the highway.";
 }
