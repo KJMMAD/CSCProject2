0cb0365f7ade84cedd0bdf8880342d60d0356476
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-10-07 13:38:23 -0400

    Code for the second program.

--- CSCProject2.cpp
+++ CSCProject2.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 
-int main()
-{
-    float mMeters = 1.5;
-    for (int year = 5; year <= 10; year++) {
-        float risenHeight = mMeters * year;
-        if (year == 5 || year == 7 || year == 10) {
-            std::cout << "The ocean level will have risen by " << risenHeight << "millimeters in " << year << " years.\n";
-        }
-    }
+int main() {
+	int gasTank = 20;
+	float mpgTown = 23.5;
+	float mpgHighway = 28.9;
+	std::cout << "A car with a 20 gallon gas tank can drive for " << gasTank * mpgTown << " in a town and " << gasTank * mpgHighway << " on the highway.";
 }
